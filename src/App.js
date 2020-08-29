b16c46a2b2b38c01a5bfa826b7e26cf5124da0df
Neil Coutinho <njcoutinho@gmail.com>
2020-08-29 08:54:56 +0530

    Simple State

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component {
+  state = {
+    greeting: `Hello World I'm from state`
+  }
 
   render() {
 
     return (
       <div className="App">
-        Hello World
+        { this.state.greeting }
         <Person name="Neil Coutinho" age="32"/>
         <Person name="Karen Coutinho" age="31"/>
         <Person name="Isabelle Coutinho" age="2"><p>I am a baby</p></Person>
