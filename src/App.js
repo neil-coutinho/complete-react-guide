9761343e239c8631b888e12d950ec1736bb64f7f
Neil Coutinho <njcoutinho@gmail.com>
2020-08-29 09:31:42 +0530

    Use arrow functions for class methods and setState for mutating state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    greeting: `Hello World I'm from state`
+    greeting: `Hello World I'm from state`,
+    persons : [
+      { name: `Neil Coutinho`, age: 32}
+    ]
+  }
+
+  onClickHandler = () => {
+
+   this.setState({
+     greeting: `I have been changed through state`
+   })
   }
 
   render() {
@@ -13,7 +23,8 @@ class App extends Component {
     return (
       <div className="App">
         { this.state.greeting }
-        <Person name="Neil Coutinho" age="32"/>
+        <button onClick={this.onClickHandler}>BUTTON</button>
+        <Person name={this.state.persons[0].name} age="32"/>
         <Person name="Karen Coutinho" age="31"/>
         <Person name="Isabelle Coutinho" age="2"><p>I am a baby</p></Person>
       </div>
