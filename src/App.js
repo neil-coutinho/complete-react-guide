03c619389f0ae1325f13e18fb1bc070192f0610d
Neil Coutinho <njcoutinho@gmail.com>
2020-08-30 08:59:17 +0530

    Using useState hook to update state in functional based components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component {
-  state = {
+const app = () => {
+
+  const [appState, setStateFn] = useState({
+      
     greeting: `Hello World I'm from state`,
     persons : [
-      { name: `Neil Coutinho`, age: 32}
-    ]
+        { name: `Neil Coutinho`, age: 32}
+      ]
+  });
+
+  //console.log(appState, setStateFn)
+
+  const onClickHandler = () => {
+    setStateFn({
+        ...appState,
+         greeting: `I have been changed through state`
+       });
   }
 
-  onClickHandler = () => {
+  return (
+    <div className="App">
+      { appState.greeting }
+      <button onClick={onClickHandler}>BUTTON</button>
+      <Person name={appState.persons[0].name} age="32"/>
+      <Person name="Karen Coutinho" age="31"/>
+      <Person name="Isabelle Coutinho" age="2"><p>I am a baby</p></Person>
+    </div>
+  )
+  // state = {
+  //   greeting: `Hello World I'm from state`,
+  //   persons : [
+  //     { name: `Person Coutinho`, age: 32}
+  //   ]
+  // }
 
-   this.setState({
-     greeting: `I have been changed through state`
-   })
-  }
+  // onClickHandler = () => {
 
-  render() {
+  //  this.setState({
+  //    greeting: `I have been changed through state`
+  //  })
+  // }
 
-    return (
-      <div className="App">
-        { this.state.greeting }
-        <button onClick={this.onClickHandler}>BUTTON</button>
-        <Person name={this.state.persons[0].name} age="32"/>
-        <Person name="Karen Coutinho" age="31"/>
-        <Person name="Isabelle Coutinho" age="2"><p>I am a baby</p></Person>
-      </div>
-    )
+  // render() {
 
-  }
+    
+
+  // }
 
 
 }
 
-export default App;
+export default app;
