a7268be17981d317bfd26584260f0a8fce1c9fba
Neil Coutinho <njcoutinho@gmail.com>
2020-08-30 09:09:11 +0530

    Child funcitonal component

---
+++ src/Bio/Bio.js
@@ -0,0 +1,11 @@
+import  React,{ useState } from 'react';
+
+const bio = (props) => {
+    return (
+      <div><h1>Bio goes here</h1></div>  
+    
+    )
+}
+
+
+export default bio;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Bio from '../Bio/Bio';
 const person = (props) => {
     return (
        <div>
         <p>Hi! my name is {props.name} and I am {props.age} years old</p>
         {props.children}
+        <Bio/>
        </div>
         
     )
