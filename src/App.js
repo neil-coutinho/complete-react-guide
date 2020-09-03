64958b3d77ec712ee5c9c0a23c88dae91d584da3
Neil Coutinho <njcoutinho@gmail.com>
2020-09-03 08:43:04 +0530

    Conditional toggle jsx with ternary operator

--- src/App.js
+++ src/App.js
@@ -6,49 +6,80 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     greeting: `Hello World I'm from state`,
-    persons : [
-      { name: `Neil Coutinho`, age: 32}
+    showPersons: false,
+    persons: [
+      { name: `Neil Coutinho`, age: 32 }
     ]
   }
 
   onClickHandler = (name) => {
     console.log(name)
-   this.setState({
-     greeting: `I have been changed through state by ${name}`
-   })
+    this.setState({
+      greeting: `I have been changed through state by ${name}`
+    })
   }
 
   onNameChangeHandler(event) {
-    
+
     this.setState({
-  
+
       persons: [{
         name: event.target.value, age: 32
       }]
     })
   }
 
+
+  togglePersons = () => {
+    
+    let currentState = this.state.showPersons;
+
+    this.setState(
+      {showPersons: !currentState
+    });
+  }
+
+
+
   render() {
 
     return (
       <div className="App">
-        { this.state.greeting }
-        <button 
-        onClick={this.onClickHandler.bind(this,"button")}>BUTTON</button>
-        <Person 
-        name={this.state.persons[0].name} 
-        age="32"
-        onNameChange={(e) => this.onNameChangeHandler(e)}
-        />
-        <Person name="Karen Coutinho" age="31"
-        onNameChange={this.onNameChangeHandler.bind(this)}
-        clickHandlerFn={() => this.onClickHandler(`Karen Coutinho`)}
-        />
-        <Person name="Isabelle Coutinho" age="2"
-        
-        onClickHandler={this.onClickHandler.bind(this,"Isabelle Coutinho")}
-        
-        ><p>I am a baby</p></Person>
+        {this.state.greeting}
+        <button
+          onClick={this.onClickHandler.bind(this, "button")}>BUTTON</button>
+
+          <p>
+            <button onClick={this.togglePersons}>Toggle Persons List</button>
+          </p>
+
+        {
+          this.state.showPersons ?
+
+            <div>
+              <Person
+                name={this.state.persons[0].name}
+                age="32"
+                onNameChange={(e) => this.onNameChangeHandler(e)}
+              />
+              <Person name="Karen Coutinho" age="31"
+                onNameChange={this.onNameChangeHandler.bind(this)}
+                clickHandlerFn={() => this.onClickHandler(`Karen Coutinho`)}
+              />
+              <Person name="Isabelle Coutinho" age="2"
+
+                onClickHandler={this.onClickHandler.bind(this, "Isabelle Coutinho")}
+
+              ><p>I am a baby</p></Person>
+            </div>
+
+
+            : null
+        }
+
+
+
+
       </div>
     )
 
