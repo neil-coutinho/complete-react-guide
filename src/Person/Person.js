1a6408c94aa010f7133ae527b6719ea137b83a3d
Neil Coutinho <njcoutinho@gmail.com>
2020-09-08 08:58:57 +0530

    Installed styled-components package and wrapped person component in a styled component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,28 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const PersonWrapper = styled.div``;
+
 
 const person = (props) => {
   //  console.log(props)
 
   const style = {
-      textAlign: 'left',
-      '@media (max-width: 767px)' : {
-        textAlign: 'center'
-      }
+    textAlign: 'left',
+    '@media (max-width: 767px)': {
+      textAlign: 'center'
+    }
   }
 
-    return (
-       <div style={style} className="Person">
+  return (
+    <PersonWrapper>
+      <div style={style} className="Person">
         <p>{props.index} => Hi! my name is {props.name} and I am {props.age} years old</p>
         {props.children}
         {/* <p><button onClick={props.onClickHandler}>Change Greeting</button> </p>*/}
-        
+
         <button onClick={props.deleteHandler}>Delete</button>
-       
-        <div><input type="text" onChange={props.nameChangeHandler} value={props.name}/></div>
-       </div>
-        
-    )
-   
+
+        <div><input type="text" onChange={props.nameChangeHandler} value={props.name} /></div>
+      </div>
+    </PersonWrapper>
+
+
+  )
+
 }
 
 
